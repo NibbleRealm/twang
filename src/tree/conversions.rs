1b71a8180a555eec47dfa0f63d57e5fa921e0a34
Jeron Aldaron Lau <aldaronlau@gmail.com>
2024-06-15 01:21:59 -0500

    Implement `float_to_int()` function

--- src/tree/conversions.rs
+++ src/tree/conversions.rs
@@ -1,4 +1,4 @@
-/// Convert unsigned 32-bit integer counter to floating point (-1 to 1)
+/// Convert signed 32-bit integer counter to floating point (-1 to 1)
 pub(crate) fn int_to_float(int: i32) -> f32 {
     // Split sign and magnitude from signed integer
     let (uint, sign_bit) = if int < 0 {
@@ -32,6 +32,21 @@ pub(crate) fn int_to_float(int: i32) -> f32 {
     f32::from_bits(sign_bit | exponent | fraction)
 }
 
+/// Convert floating point (-1 to 1) to signed 32-bit integer
+pub(crate) fn float_to_int(float: f32) -> i32 {
+    let float = float.to_bits();
+    let sign = (float & (1 << 31)) >> 31;
+    let int = ((float << 9) >> 9) | (1 << 24);
+    let exponent = (float << 1) >> 24;
+    let shift = std::dbg!(127u32.saturating_sub(exponent));
+
+    if shift >= 32 {
+        0
+    } else {
+        ((int as i32) * (1 - (sign as i32 * 2))).saturating_mul(128) >> shift
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,5 +71,23 @@ mod tests {
         assert_eq!(int_to_float(i32::MIN / 4), -0.25);
         assert_eq!(int_to_float(i32::MIN / 2), -0.5);
         assert_eq!(int_to_float(i32::MIN), -1.0);
+
+        assert_eq!(float_to_int(0.0), 0);
+        assert_eq!(float_to_int(-1.0), i32::MIN);
+        assert_eq!(float_to_int(-0.5), i32::MIN / 2);
+        assert_eq!(float_to_int(-0.25), i32::MIN / 4);
+        //assert_eq!(float_to_int(4.1909516e-9), 4);
+        assert_eq!(float_to_int(3.259629e-9), 3);
+        //assert_eq!(float_to_int(2.3283064e-9), 2);
+        assert_eq!(float_to_int(1.3969839e-9), 1);
+        assert_eq!(float_to_int(2.0f32.powf(-31.0)), 0);
+        assert_eq!(float_to_int(-2.0f32.powf(-31.0)), -1);
+        assert_eq!(float_to_int(-1.3969839e-9), -2);
+        //assert_eq!(float_to_int(-2.3283064e-9), -3);
+        assert_eq!(float_to_int(-3.259629e-9), -4);
+        //assert_eq!(float_to_int(-4.1909516e-9), -5);
+        assert_eq!(float_to_int(0.25), i32::MAX / 4);
+        assert_eq!(float_to_int(0.5), i32::MAX / 2);
+        assert_eq!(float_to_int(1.0), i32::MAX);
     }
 }
