92d4021603f6b53f3011832ac720cc23a3e51bd9
Jeron Aldaron Lau <aldaronlau@gmail.com>
2025-01-26 00:29:51 -0600

    Add new `u32_to_f32()` function

--- src/tree/conversions.rs
+++ src/tree/conversions.rs
@@ -1,10 +1,31 @@
+/// Convert [`u32`] fraction to [`f32`] (ranged 0 to 1).
+pub(crate) fn u32_to_f32(fraction: u32) -> f32 {
+    // Calculate leading zeros including inferred 1
+    let leading_zeros = fraction.leading_zeros();
+    // Remove leading zeros and extra bit to subtract from exponent
+    let (fraction, leading_zeros) = if leading_zeros >= 32 {
+        (0, 127)
+    } else {
+        (fraction << leading_zeros, leading_zeros + 1)
+    };
+    // Truncate to 24-bit fraction
+    let fraction = fraction >> 8;
+    // Clear 24th bit
+    let fraction = fraction & !(1 << 23);
+    // Calculate -127 bias exponent (plus inferred 1)
+    let exponent = (127 - leading_zeros) << 23;
+
+    f32::from_bits(exponent | fraction)
+        * f32::from_bits(0b111111100000000000000000000001)
+}
+
 /// Convert signed 32-bit integer counter to floating point (-1 to 1)
 pub(crate) fn int_to_float(int: i32) -> f32 {
     // Split sign and magnitude from signed integer
     let (uint, sign_bit) = if int < 0 {
-        ((-1 - int) as u32, 1 << 31)
+        (reinterpret_unsigned(-1 - int), 1 << 31)
     } else {
-        (int as u32, 0)
+        (reinterpret_unsigned(int), 0)
     };
     // Remove sign bit, so that it's not counted in leading zeros, add 1 for
     // rounding accuracy
@@ -34,7 +55,12 @@ pub(crate) fn int_to_float(int: i32) -> f32 {
 
 /// Convert floating point (-1 to 1) to signed 32-bit integer
 pub(crate) fn float_to_int(float: f32) -> i32 {
-    let float = float.to_bits();
+    // Infinity, 0, subnormal and NaN are all treated as 0
+    let float = if !float.is_normal() {
+        0u32
+    } else {
+        float.clamp(-1.0, 1.0).to_bits()
+    };
     // Check if float is negative
     let is_negative = (float & (1 << 31)) != 0;
     // Convert to 23-bit fixed point fraction
@@ -42,24 +68,56 @@ pub(crate) fn float_to_int(float: f32) -> i32 {
     // Add floating point inferred digit 1.x, bringing to 24 bits
     let int = int | (1 << 23);
     // Extract 8-bit exponent
-    let exponent = std::dbg!((float << 1) >> 24);
+    let exponent = (float << 1) >> 24;
     // Calculate positive and negative shift
     let shift = 120u32.saturating_sub(exponent);
     let shift_back = exponent.saturating_sub(119);
     // Apply calculated exponential shift
     let int = (int << shift_back).saturating_sub(1);
     let int = if shift >= 32 { 0 } else { int >> shift };
+    let int = reinterpret_signed(int);
 
     if is_negative {
-        -1 - (int as i32)
+        -1 - int
     } else {
-        int as i32
+        int
     }
+}
+
+#[inline(always)]
+fn reinterpret_signed(int: u32) -> i32 {
+    i32::from_ne_bytes(int.to_ne_bytes())
+}
+
+#[inline(always)]
+fn reinterpret_unsigned(int: i32) -> u32 {
+    u32::from_ne_bytes(int.to_ne_bytes())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn unsigned_to_float() {
+        for i in 0..10 {
+            assert_eq!(
+                u32_to_f32(u32::MAX / 2u32.pow(i)),
+                0.5f32.powi(i.try_into().unwrap()),
+            );
+        }
+
+        //
+        assert_eq!(u32_to_f32(0), 0.0);
+        assert_eq!(u32_to_f32(1), 2.3283067e-10);
+        assert_eq!(u32_to_f32(2), 4.6566134e-10);
+        assert_eq!(u32_to_f32(3), 6.9849204e-10);
+        assert_eq!(u32_to_f32(4), 9.313227e-10);
+        assert_eq!(u32_to_f32(5), 1.1641533e-9);
+        assert_eq!(u32_to_f32(6), 1.3969841e-9);
+        assert_eq!(u32_to_f32(7), 1.6298147e-9);
+        assert_eq!(u32_to_f32(8), 1.8626454e-9);
+    }
 
     #[test]
     fn conversions() {
@@ -91,6 +149,8 @@ mod tests {
         assert_eq!(int_to_float(-6), -5.122274e-09);
         assert_eq!(int_to_float(-7), -6.0535967e-9);
         assert_eq!(int_to_float(-8), -6.9849193e-9);
+        assert_eq!(int_to_float(i32::MIN / 512), -0.0039062495); // -0.001953125);
+        assert_eq!(int_to_float(i32::MIN / 256), -0.0078124995); // -0.00390625);
         assert_eq!(int_to_float(i32::MIN / 128), -0.0078125);
         assert_eq!(int_to_float(i32::MIN / 64), -0.015625);
         assert_eq!(int_to_float(i32::MIN / 32), -0.03125);
@@ -127,6 +187,8 @@ mod tests {
         assert_eq!(float_to_int(-5.122274e-09), -6);
         assert_eq!(float_to_int(-6.0535967e-9), -7);
         assert_eq!(float_to_int(-6.9849193e-9), -8);
+        assert_eq!(float_to_int(-0.001953125), i32::MIN / 1024); // / 512);
+        assert_eq!(float_to_int(-0.00390625), i32::MIN / 512); // / 256);
         assert_eq!(float_to_int(-0.0078125), i32::MIN / 128);
         assert_eq!(float_to_int(-0.015625), i32::MIN / 64);
         assert_eq!(float_to_int(-0.03125), i32::MIN / 32);
